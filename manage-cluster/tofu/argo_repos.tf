ecab2b08c15b48771f688277bd37effc89f6a407
Chase 李 <chase@emiya.io>
2024-09-08 13:26:43 -0500

    Adding Github repo to Argo

--- manage-cluster/tofu/argo_repos.tf
+++ manage-cluster/tofu/argo_repos.tf
@@ -8,12 +8,33 @@ resource "kubernetes_manifest" "secret_argocd_bitnami_repo" {
         "argocd.argoproj.io/secret-type" = "repository"
       }
       "name" = "bitnami-repo"
-      "namespace" = "argocd"
+      "namespace" = kubernetes_namespace.argocd.metadata[0].name
     }
     "stringData" = {
       "name" = "bitnami"
       "type" = "helm"
       "url" = "https://charts.bitnami.com/bitnami"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "secret_argocd_github_repo" {
+  computed_fields = ["stringData"]
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Secret"
+    "metadata" = {
+      "labels" = {
+        "argocd.argoproj.io/secret-type" = "repository"
+      }
+      "name" = "github"
+      "namespace" = kubernetes_namespace.argocd.metadata[0].name
+    }
+    "stringData" = {
+      "type" = "git"
+      "name" = "github"
+      "url" = "git@github.com:clee231/proxmox-setup.git"
+      "sshPrivateKey": file("../argo-ssh-key")
     }
   }
 }
